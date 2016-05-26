7a03775f45f4aa9e0d617fdcc1fa34f42d069c41
Vijay <vijaymahantesh.desai2@gmail.com>
2016-05-26 19:01:10 +0530

    App uses content updated

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var path = require('path');
 
 app.use(express.static(__dirname + '/'));
 
-// viewed at http://localhost:8090
+// viewed at http://localhost:8020
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + 'index.html'));
 });
 
-app.listen(8090);
+app.listen(8020, function(){
+    console.log('App is running on port 8020');
+});
